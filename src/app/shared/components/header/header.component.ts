39326c3a035fb970c858e6e80d13c20cc6edd9d2
Nagaraju Kasarla <nagarajukasarla79@gmail.com>
2026-06-12 22:25:05 +0530

    Added search field

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  searchQuery = '';
+
+  onSearch() {
+    console.log('Searching for:', this.searchQuery);
+  }
+
+  clearSearch() {
+    this.searchQuery = '';
+  }
+}
+
